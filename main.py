0cabbf6e30473f95fca89770df368f315a81e649
IrmaM00 <mesarosirma7@gmail.com>
2025-03-14 14:48:23 +0100

    Dodavanje klase Cart za upravljanje korpom

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from product import Product
 from product_manager import ProductManager
+from cart import Cart
 
 # Kreirajte instancu ProductManager
 manager = ProductManager()
@@ -9,10 +10,27 @@ product1 = Product("Laptop", 1000, 5)
 product2 = Product("Phone", 700, 10)
 product3 = Product("Headphones", 150, 15)
 
+
 manager.add_product(product1)
 manager.add_product(product2)
 manager.add_product(product3)
 
 # Prikaz proizvoda i ukupne vrednosti inventara
+
 manager.display_products()
 print("Total inventory value:", manager.total_inventory_value())
+
+# Kreiranje instance za Cart
+cart = Cart()
+
+# Dodavanje proizvoda u korpu
+
+cart.add_to_cart(manager.products[0])
+cart.add_to_cart(manager.products[1])
+cart.add_to_cart(manager.products[2])
+cart.add_to_cart(manager.products[3])
+
+# Prikaz korpe i ukupne vrednosti 
+
+cart.display_cart()
+print(f"Ukupna vrednost korpe: {cart.calculate_total()}")
